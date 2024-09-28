46faaecc0956c06726092cd6d574623897a4265f
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-28 15:57:48 -0400

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return 'ERROR';
+    else if (typeof start != 'number' || typeof end != 'number') return 'ERROR';
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    else {
+        let result = 0;
+        if (start <= end) for (let i = start; i <= end; i++) result += i;
+        else if (end < start) for (let i = end; i <= start; i++) result += i;
+        return result;
+    }
 };
 
 // Do not edit below this line

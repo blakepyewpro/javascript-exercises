3b42b1f224cfa5bb5087233427e522dd607db05a
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-28 16:37:49 -0400

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5 / 9);
+  result = Number(result.toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = temp * (9 / 5) + 32;
+  result = Number(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line

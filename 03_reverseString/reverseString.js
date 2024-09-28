2edda9fefd4c4141873b6fe06c441b314ac50cf7
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-28 14:52:41 -0400

    complete 03_reverseString didn't use reverse because that would be easy

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string == '') return '';
+    else {
+        let resultArray = [];
+        for (let i = 1; i <= string.length; i++) {
+            resultArray.push(string.at(-i));
+        }
+        return resultArray.join('');
+    }
 };
 
 // Do not edit below this line

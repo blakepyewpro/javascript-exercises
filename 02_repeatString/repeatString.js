b0ff70cfdcc4c65f79ac0ff238339787ed928b3e
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-28 14:25:32 -0400

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = '';
+    let iterator = 0;
 
+    if (num == 0) return result;
+    else if (num < 0) return 'ERROR';
+    else if (string == '') return result;
+    else while (iterator < num) {
+        result += string;
+        iterator++;
+    }
+    return result;
 };
 
 // Do not edit below this line

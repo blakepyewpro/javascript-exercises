ba25e0144e8a1bca89811199047346e998df0195
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-09-28 15:25:14 -0400

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = array;
+    for (const arg of args) {
+        while (result.includes(arg)) {
+            result.splice(result.indexOf(arg), 1);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

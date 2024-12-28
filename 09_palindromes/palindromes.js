885efba3ada690b9e94d8672937f11c3492468b1
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-12-28 14:15:55 -0500

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let filteredArray = input.toLowerCase().split("").filter(isAlphabetic);
+  let reversedArray = filteredArray.toReversed();
 
+  if (filteredArray.join("") == reversedArray.join("")) return true;
+  else return false;
+};
+
+function isAlphabetic(str) {
+  const filter = " !,.?";
+  if (filter.includes(str)) return false;
+  else return true;
 };
 
 // Do not edit below this line

d03f4bfc8418349436c6f9c0d2c793f5e3fe48bc
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2024-12-30 21:18:27 -0500

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number == 0) {
+    return 0;
+  } else if (number == 1) {
+    return 1;
+  } else if (number < 0) {
+    return "OOPS";
+  } else {
+    let fib1 = 0;
+    let fib2 = 1;
+    let result;
+    for (let i = 1; i < number; i++) {
+      result = fib1 + fib2;
+      fib1 = fib2;
+      fib2 = result;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
